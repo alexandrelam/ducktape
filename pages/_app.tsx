f2454cec50a32cc3123e7f724c282219c16a76fd
Alexandre Lam <alexandrelam@outlook.com>
2022-11-07 22:02:15 -0500

    feat: duck colors (#11)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,27 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
 import type { AppProps } from "next/app";
 import { AppLayout } from "../layouts/AppLayout";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#01AB46",
+      },
+      secondary: {
+        main: "#f5f5f5",
+      },
+    },
+  });
+
   return (
-    <AppLayout>
-      <Component {...pageProps} />
-    </AppLayout>
+    <ThemeProvider theme={theme}>
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    </ThemeProvider>
   );
 }
 
